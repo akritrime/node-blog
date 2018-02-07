0f977e3c11280ec1bd299868d61f5d25297e58ec
akritrime <akritrime@gmail.com>
2018-02-07 11:33:03 +0530

    changed posts/:id query to findById

--- src/routes/posts/_middlewares.ts
+++ src/routes/posts/_middlewares.ts
@@ -14,15 +14,14 @@ export const getAll: IMiddleware = async (ctx, next) => {
 export const getOne: IMiddleware = async (ctx, next) => {
     try {
         const id = ctx.params.id
-        const post = await Post.query()
-            .where("id", id)
+        const post = await Post.query().findById(id)
         // console.log(post)
-        if(post.length == 0) {
+        if(!post) {
             const err = new ErrorWithStatus(`Post with id ${id} doesn't exist.`)
             err.status = 404
             throw err
         }
-        success(ctx, post[0])
+        success(ctx, post)
     } catch (err) {
         // console.error(err)
         error(ctx, err, "Error querying for post")
@@ -31,7 +30,8 @@ export const getOne: IMiddleware = async (ctx, next) => {
 
 export const post: IMiddleware = async (ctx, next) => {
     try {
-        
+        const { title, content } = ctx.request.body
+        Post.query().insert({})
     } catch (err) {
         
     }
